d8ff7ff929e4b5ae1d5eaa4c89d47780d1a94181
NY-2023 <146630556+NY-2023@users.noreply.github.com>
2023-10-01 17:28:33 +0500

    Create 01.metrics.sql

---
+++ .postgres/01.metrics.sql
@@ -0,0 +1,5 @@
+INSERT INTO metric (id, name, description) VALUES
+  ('dbf63a67-b027-4cff-babc-566588a199b4', 'e1.1.1', 'Scope 1 Emissions (tCO2e)'),
+  ('c075c759-2051-45fd-abe0-1858fcd0af94', 'e1.2.1', 'Scope 2 Emissions (tCO2e)'),
+  ('6e609012-5df4-426a-a3ad-f06e77632402', 'e1.3.1', 'Scope 3 Emissions (tCO2e)'),
+  ('b6b65917-e52c-4e50-aa42-32ceb579eeeb', 'e1.4.1', 'Net-Zero Target');
